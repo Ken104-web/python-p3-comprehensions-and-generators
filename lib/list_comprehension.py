a9312f326eaebd268b564a8bdda1976cc81f2e89
KenCorp <kennethwarui35@gmail.com>
2023-08-18 15:53:17 +0300

    Completed test

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_string = []
+    for string in sentence_list:
+        exclamation_string.append(string + '!')
+    return exclamation_string
